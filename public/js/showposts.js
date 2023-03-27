5208164dabb46c34368131ca67535660f07fb99d
IrisWRX <wxu64@my.bcit.ca>
2023-03-26 17:45:04 -0700

    Let my posts read from Firebase and able to change the status of the listing

---
+++ public/js/showposts.js
@@ -0,0 +1,39 @@
+function displayUserPosts() {
+  firebase.auth().onAuthStateChanged(async (user) => {
+    if (!user) {
+      console.log("No user is signed in");
+      window.location.href = "login.html";
+      return;
+    }
+
+    const postsContainer = document.getElementById("posts-go-here");
+    const postCardTemplate = document.getElementById("postCardTemplate");
+    const currentUser = firebase.firestore().collection("users").doc(user.uid);
+    const querySnapshot = await currentUser.collection("posts").get();
+
+    querySnapshot.forEach((doc) => {
+      const post = doc.data();
+      const card = postCardTemplate.content.cloneNode(true);
+
+      card.querySelector(".card-title").textContent = `Hosted by ${post.name}`;
+      card.querySelector(".card-status").textContent = post.status;
+      card.querySelector(".card-img-top").src =
+        post.image || "https://via.placeholder.com/150";
+
+      const changeStatusBtn = card.querySelector(".change-status-btn");
+      const cardStatus = card.querySelector(".card-status");
+
+      changeStatusBtn.addEventListener("click", async () => {
+        post.status = post.status === "Available" ? "Unavailable" : "Available";
+        cardStatus.textContent = post.status;
+
+        // Update the status in Firestore
+        await doc.ref.update({ status: post.status });
+      });
+
+      postsContainer.appendChild(card);
+    });
+  });
+}
+
+displayUserPosts();
