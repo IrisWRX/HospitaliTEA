3cc1eb5761cbf39a3f1f889305f243b461c8c710
Steven Lai <tinleaves.0@gmail.com>
2023-03-28 21:56:56 -0700

    added delete button for posts with modal pop-up for confirmation, modified showposts.js

--- public/js/showposts.js
+++ public/js/showposts.js
@@ -15,12 +15,12 @@ function displayUserPosts() {
       const post = doc.data();
       const card = postCardTemplate.content.cloneNode(true);
 
-      card.querySelector(".card-title").textContent = `Hosted by you`;
+      card.querySelector(".card-title").textContent = post.title;
       card.querySelector(".card-status").textContent = post.status;
-      card.querySelector(".card-img-top").src =
-        post.image || "https://via.placeholder.com/150";
+      card.querySelector(".card-img-top").src = post.image || "https://via.placeholder.com/150";
 
       const changeStatusBtn = card.querySelector(".change-status-btn");
+      const deleteButton = card.querySelector(".delete-button");
       const cardStatus = card.querySelector(".card-status");
 
       changeStatusBtn.addEventListener("click", async () => {
@@ -30,6 +30,35 @@ function displayUserPosts() {
         // Update the status in Firestore
         await doc.ref.update({ status: post.status });
       });
+
+      deleteButton.addEventListener("click", () => {
+        const postId = deleteButton.dataset.postId;
+        const modal = document.getElementById("delete-modal");
+        const cancelBtn = document.getElementById('cancel-btn');
+        const confirmDeleteButton = document.getElementById("confirm-delete");
+        modal.style.display = "block";
+
+        cancelBtn.addEventListener('click', () => {
+          modal.style.display = 'none';
+          document.getElementById('confirm-delete').checked = false;
+        });
+
+        window.addEventListener('click', (event) => {
+          if (event.target == modal) {
+            modal.style.display = 'none';
+            document.getElementById('confirm-delete').checked = false;
+          }
+        });
+
+        confirmDeleteButton.addEventListener("click", async () => {
+          await doc.ref.delete();
+          modal.remove();
+          location.reload();
+          modal.style.display = "none";
+        });
+      });
+
+      deleteButton.dataset.postId = doc.id;
 
       postsContainer.appendChild(card);
     });
