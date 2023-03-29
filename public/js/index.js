2afdfe1c342814cd1da8c4fe1d2606422112c7ae
IrisWRX <wxu64@my.bcit.ca>
2023-03-28 20:22:05 -0700

    Changed login page and styled it

---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+document
+  .getElementById("get-started-btn")
+  .addEventListener("click", function () {
+    window.location.href = "login.html"; // Update this to the correct path of your login page
+  });
